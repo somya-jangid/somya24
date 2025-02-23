64274bd1669cffadce407f586ccaad60c346c2e9
somya-jangid <somyajangid78@gmail.com>
2025-02-24 01:50:25 +0530

    Add or update backend/middleware/auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,1 @@
+const jwt = require('jsonwebtoken');const auth = (req, res, next) => { const token = req.header('Authorization').replace('Bearer ', ''); try { const decoded = jwt.verify(token, 'secret'); req.user = decoded; next(); } catch (error) { res.status(401).send({ error: 'Please authenticate.' }); }};module.exports = auth;
